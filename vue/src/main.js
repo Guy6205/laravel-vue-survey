1e780d46fcbbe31d6f94cdf70b5d493513e38fa9
Guy Smith <guy.smith6205@gmail.com>
2023-04-13 15:08:10 +1200

    Installed TailwindCSS & updated tailwind.config + postcss.config

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
+import store from './store'
+import './index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .mount('#app')
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,7 +1,12 @@
 import {createStore} from 'vuex';
 
 const store = createStore({
-    state: {},
+    state: {
+        user: {
+            data: {name: 'John Doe'},
+            token: null
+        }
+    },
     getters: {},
     actions: {},
     mutations: {},
