62ad5f0744848e786d4dab32f3ca5ecf0069d00d
Guy Smith <guy.smith6205@gmail.com>
2023-04-13 14:26:50 +1200

    testing

---
+++ vue/src/store/index.js
@@ -0,0 +1,11 @@
+import {createStore} from 'vuex';
+
+const store = createStore({
+    state: {},
+    getters: {},
+    actions: {},
+    mutations: {},
+    modules: {}
+});
+
+export default store;
