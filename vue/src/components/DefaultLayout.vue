f41fb31714a9b92abf05930625c26ec818595c14
Guy Smith <guy.smith6205@gmail.com>
2023-04-14 10:36:54 +1200

    Updated nav bar UI

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -9,16 +9,23 @@
               </div>
               <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                  <router-link
+                    v-for="item in navigation" 
+                    :key="item.name" 
+                    :to="item.to" 
+                    active-class="bg-gray-900 text-white"
+                    :class="[
+                      this.$route.name === item.to.name
+                      ? '' 
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white', 
+                      'rounded-md px-3 py-2 text-sm font-medium'
+                    ]" 
+                  >{{ item.name }}</router-link>
                 </div>
               </div>
             </div>
             <div class="hidden md:block">
               <div class="ml-4 flex items-center md:ml-6">
-                <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span class="sr-only">View notifications</span>
-                  <BellIcon class="h-6 w-6" aria-hidden="true" />
-                </button>
   
                 <!-- Profile dropdown -->
                 <Menu as="div" class="relative ml-3">
@@ -30,8 +37,14 @@
                   </div>
                   <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                     <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                        <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                      <MenuItem 
+                        v-slot="{ active }"
+                      >
+                        <a 
+                        @click="logout"
+                        :class="[
+                          'block px-4 py-2 text-sm text-gray-700 cursor-pointer',
+                        ]">Sign Out</a>
                       </MenuItem>
                     </MenuItems>
                   </transition>
@@ -51,7 +64,16 @@
   
         <DisclosurePanel class="md:hidden">
           <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+            <router-link 
+              v-for="item in navigation" 
+              :to="item.to" 
+              active-class="bg-gray-900 text-white"
+              :class="[
+                this.$route.name === item.to.name
+                ? '' 
+                : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium'
+              ]" 
+            >{{ item.name }}</router-link>
           </div>
           <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">
@@ -62,13 +84,13 @@
                 <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
                 <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
               </div>
-              <button type="button" class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="h-6 w-6" aria-hidden="true" />
-              </button>
             </div>
             <div class="mt-3 space-y-1 px-2">
-              <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">{{ item.name }}</DisclosureButton>
+              <DisclosureButton 
+                as="a"
+                @click="logout" 
+                class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white cursor-pointer"
+              >Sign Out</DisclosureButton>
             </div>
           </div>
         </DisclosurePanel>
@@ -88,16 +110,8 @@
   import { computed } from 'vue';
   
   const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
-  ]
-  const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
+    { name: 'Dashboard', to: { name: 'Dashboard' } },
+    { name: 'Surveys', to: { name: 'Surveys' } },
   ]
 
   export default {
@@ -118,7 +132,6 @@
       return {
         user: computed(() => store.state.user.data),
         navigation,
-        userNavigation,
       };
     }
   };
