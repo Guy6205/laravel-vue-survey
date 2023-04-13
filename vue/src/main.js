aa9ab645aae6cbee616ca0beabe91d50e003b195
Guy Smith <guy.smith6205@gmail.com>
2023-04-13 16:46:26 +1200

    Created router & set up basic views for Login, Register & Dashboard

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
-// import './style.css'
 import store from './store'
+import router from './router'
 import './style.css'
 import App from './App.vue'
 
 createApp(App)
     .use(store)
+    .use(router)
     .mount('#app')
