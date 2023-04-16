ab20049e695a8c49c0bebf913db0b7fb29d6afad
Guy6205 <65946520+Guy6205@users.noreply.github.com>
2023-04-17 11:52:07 +1200

    Revert "Develop"

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,12 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
-import store from './store'
-import router from './router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App)
-    .use(store)
-    .use(router)
-    .mount('#app')
+createApp(App).mount('#app')
